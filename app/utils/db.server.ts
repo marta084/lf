39d2586b5ac1cccd23fa411b4ec5f07f48e443aa
marta00084 <martvonk00084@gmail.com>
2024-01-27 15:09:47 +0100

    Update db.server.ts

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/edge';
 import { PrismaLibSQL } from '@prisma/adapter-libsql';
 import { createClient } from '@libsql/client';
 
