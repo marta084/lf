42063452e99e22324566a2da21d3011789fe308c
marta00084 <martvonk00084@gmail.com>
2024-01-27 12:42:35 +0100

    Update db.server.ts

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,6 +1,6 @@
-import { PrismaClient } from '@prisma/client/edge';
+import { PrismaClient } from '@prisma/client';
 import { PrismaLibSQL } from '@prisma/adapter-libsql';
-import { createClient } from '@libsql/client/web';
+import { createClient } from '@libsql/client';
 
 const prismaClientSingleton = () => {
   const libsql = createClient({
