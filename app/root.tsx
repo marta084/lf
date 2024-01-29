98d3f48fc8df46e1cc9c6df335610c10e06168be
marta00084 <martvonk00084@gmail.com>
2024-01-29 18:48:23 +0100

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -1,25 +1,24 @@
-
 import { cssBundleHref } from '@remix-run/css-bundle'
 import {
-	type ActionFunctionArgs,
-	type LoaderFunctionArgs,
-	json,
-	type LinksFunction,
-	type MetaFunction,
+  type ActionFunctionArgs,
+  type LoaderFunctionArgs,
+  json,
+  type LinksFunction,
+  type MetaFunction,
 } from '@remix-run/cloudflare'
 
 import {
-	Links,
-	LiveReload,
-	Meta,
-	Outlet,
-	Scripts,
-	ScrollRestoration,
-	useFetcher,
-	useFetchers,
-	useLoaderData,
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useFetcher,
+  useFetchers,
+  useLoaderData,
+  useMatches,
 } from '@remix-run/react'
-
 
 import { AuthenticityTokenProvider } from 'remix-utils/csrf/react'
 import MartaBlogFavicon from './assets/favicon.ico'
@@ -49,236 +48,232 @@ import { useEffect } from 'react'
 import { Spacer } from './components/spacer'
 
 export const links: LinksFunction = () => {
-	return [
-		...(cssBundleHref
-			? [{ rel: 'stylesheet', href: rdtStylesheet }]
-			: []),
-		{ rel: 'icon', href: MartaBlogFavicon, type: 'image/x-icon' },
-		{ rel: 'stylesheet', href: tailwindStyleSheet },
+  return [
+    ...(cssBundleHref ? [{ rel: 'stylesheet', href: rdtStylesheet }] : []),
+    { rel: 'icon', href: MartaBlogFavicon, type: 'image/x-icon' },
+    { rel: 'stylesheet', href: tailwindStyleSheet },
 
-		cssBundleHref ? { rel: 'stylesheet', href: cssBundleHref } : null,
-	].filter(Boolean)
+    cssBundleHref ? { rel: 'stylesheet', href: cssBundleHref } : null,
+  ].filter(Boolean)
 }
 
 export async function loader({ request }: LoaderFunctionArgs) {
-	const [csrfToken, csrfCookieHeader] = await csrf.commitToken(request)
-	
-	const toastCookieSession = await toastSessionStorage.getSession(
-		request.headers.get('cookie'),
-	)
-	const toast = toastCookieSession.get('toast')
-	console.log('allgooooood')
-	return json(
-		{
-			
-			theme: getTheme(request),
-			toast,
-			ENV: getEnv(),
-			csrfToken,
-			
-		},
-		{
-			headers: combineHeaders(
-				csrfCookieHeader ? { 'set-cookie': csrfCookieHeader } : null,
-				{
-					'set-cookie':
-						await toastSessionStorage.commitSession(toastCookieSession),
-				},
-			),
-		},
-	)
+  const [csrfToken, csrfCookieHeader] = await csrf.commitToken(request)
+
+  const toastCookieSession = await toastSessionStorage.getSession(
+    request.headers.get('cookie'),
+  )
+  const toast = toastCookieSession.get('toast')
+  console.log('allgooooood')
+  return json(
+    {
+      theme: getTheme(request),
+      toast,
+      ENV: getEnv(),
+      csrfToken,
+    },
+    {
+      headers: combineHeaders(
+        csrfCookieHeader ? { 'set-cookie': csrfCookieHeader } : null,
+        {
+          'set-cookie':
+            await toastSessionStorage.commitSession(toastCookieSession),
+        },
+      ),
+    },
+  )
 }
 
 const ThemeFormSchema = z.object({
-	theme: z.enum(['light', 'dark']),
+  theme: z.enum(['light', 'dark']),
 })
 
 export async function action({ request }: ActionFunctionArgs) {
-	const formData = await request.formData()
-	invariantResponse(
-		formData.get('intent') === 'update-theme',
-		'Invalid intent',
-		{ status: 400 },
-	)
-	const submission = parse(formData, {
-		schema: ThemeFormSchema,
-	})
-	if (submission.intent !== 'submit') {
-		return json({ status: 'success', submission } as const)
-	}
-	if (!submission.value) {
-		return json({ status: 'error', submission } as const, { status: 400 })
-	}
-	const { theme } = submission.value
+  const formData = await request.formData()
+  invariantResponse(
+    formData.get('intent') === 'update-theme',
+    'Invalid intent',
+    { status: 400 },
+  )
+  const submission = parse(formData, {
+    schema: ThemeFormSchema,
+  })
+  if (submission.intent !== 'submit') {
+    return json({ status: 'success', submission } as const)
+  }
+  if (!submission.value) {
+    return json({ status: 'error', submission } as const, { status: 400 })
+  }
+  const { theme } = submission.value
 
-	const responseInit = {
-		headers: { 'set-cookie': setTheme(theme) },
-	}
-	return json({ success: true, submission }, responseInit)
+  const responseInit = {
+    headers: { 'set-cookie': setTheme(theme) },
+  }
+  return json({ success: true, submission }, responseInit)
 }
 
 function Document({
-	children,
-	theme,
+  children,
+  theme,
 }: Readonly<{
-	children: React.ReactNode
-	theme?: Theme
+  children: React.ReactNode
+  theme?: Theme
 }>) {
-	return (
-		<html lang="en" className={`${theme} h-full overflow-x-hidden`}>
-			<head>
-				<Meta />
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<Links />
-			</head>
-			<body className="min-h-screen bg-background text-foreground">
-				<div className=" bg-black text-foreground">
-					<div className="max-w-4xl mx-auto flex flex-col">
-						<div className="flex justify-between">
-							<div></div>
-							<div>
-								<ThemeSwitch userPreference={theme} />
-							</div>
-						</div>
-					</div>
-				</div>
+  const matches = useMatches()
+  const includeScripts = matches.some(match => match.handle?.hydrate)
 
-				{children}
+  return (
+    <html lang="en" className={`${theme} h-full overflow-x-hidden`}>
+      <head>
+        <Meta />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Links />
+      </head>
+      <body className="min-h-screen bg-background text-foreground">
+        <div className=" bg-black text-foreground">
+          <div className="mx-auto flex max-w-4xl flex-col">
+            <div className="flex justify-between">
+              <div></div>
+              <div>
+                <ThemeSwitch userPreference={theme} />
+              </div>
+            </div>
+          </div>
+        </div>
 
-				<Toaster
-					expand
-					visibleToasts={90}
-					toastOptions={{
-						unstyled: false,
-						classNames: {
-							toast: '',
-							title: 'text-red-400',
-							description: 'text-red-400',
-							actionButton: 'bg-zinc-400',
-							cancelButton: 'bg-white',
-							closeButton: 'bg-grey-400',
-						},
-					}}
-					closeButton
-					position="bottom-right"
-					dir="rtl"
-				/>
+        {children}
 
-				<ScrollRestoration />
-				<Scripts />
-				<LiveReload />
-			</body>
-		</html>
-	)
+        <Toaster
+          expand
+          visibleToasts={90}
+          toastOptions={{
+            unstyled: false,
+            classNames: {
+              toast: '',
+              title: 'text-red-400',
+              description: 'text-red-400',
+              actionButton: 'bg-zinc-400',
+              cancelButton: 'bg-white',
+              closeButton: 'bg-grey-400',
+            },
+          }}
+          closeButton
+          position="bottom-right"
+          dir="rtl"
+        />
+
+        <ScrollRestoration />
+        {includeScripts ? <Scripts /> : null}
+        <LiveReload />
+      </body>
+    </html>
+  )
 }
 function App() {
-	const data = useLoaderData<typeof loader>()
-	const theme = useTheme()
-	return (
-		<Document theme={theme}>
-			<Header />
-			<div className="max-w-4xl mx-auto flex flex-col bg-background px-4 py-2">
-				<Outlet />
-				<Spacer size="3xs" />
+  const data = useLoaderData<typeof loader>()
+  const theme = useTheme()
+  return (
+    <Document theme={theme}>
+      <Header />
+      <div className="mx-auto flex max-w-4xl flex-col bg-background px-4 py-2">
+        <Outlet />
+        <Spacer size="3xs" />
 
-				{data.toast ? <ShowToast toast={data.toast} /> : null}
-			</div>
-			<Footer />
-		</Document>
-	)
+        {data.toast ? <ShowToast toast={data.toast} /> : null}
+      </div>
+      <Footer />
+    </Document>
+  )
 }
 
-
 export default function AppWithProviders() {
-	const data = useLoaderData<typeof loader>()
-	return (
-		<AuthenticityTokenProvider token={data.csrfToken}>
-			
-				<App />
-			
-		</AuthenticityTokenProvider>
-	)
+  const data = useLoaderData<typeof loader>()
+  return (
+    <AuthenticityTokenProvider token={data.csrfToken}>
+      <App />
+    </AuthenticityTokenProvider>
+  )
 }
 
 function useTheme() {
-	const data = useLoaderData<typeof loader>()
-	const fetchers = useFetchers()
-	const fetcher = fetchers.find(
-		f => f.formData?.get('intent') === 'update-theme',
-	)
-	const optimisticTheme = fetcher?.formData?.get('theme')
-	if (optimisticTheme === 'light' || optimisticTheme === 'dark') {
-		return optimisticTheme
-	}
-	return data.theme
+  const data = useLoaderData<typeof loader>()
+  const fetchers = useFetchers()
+  const fetcher = fetchers.find(
+    f => f.formData?.get('intent') === 'update-theme',
+  )
+  const optimisticTheme = fetcher?.formData?.get('theme')
+  if (optimisticTheme === 'light' || optimisticTheme === 'dark') {
+    return optimisticTheme
+  }
+  return data.theme
 }
 
 function ThemeSwitch({ userPreference }: Readonly<{ userPreference?: Theme }>) {
-	const fetcher = useFetcher<typeof action>()
+  const fetcher = useFetcher<typeof action>()
 
-	const [form] = useForm({
-		id: 'theme-switch',
-		lastSubmission: fetcher.data?.submission,
-		onValidate({ formData }) {
-			return parse(formData, { schema: ThemeFormSchema })
-		},
-	})
+  const [form] = useForm({
+    id: 'theme-switch',
+    lastSubmission: fetcher.data?.submission,
+    onValidate({ formData }) {
+      return parse(formData, { schema: ThemeFormSchema })
+    },
+  })
 
-	const mode = userPreference ?? 'light'
-	const nextMode = mode === 'light' ? 'dark' : 'light'
-	const modeLabel = {
-		light: 'Light ',
-		dark: 'Dark ',
-	}
+  const mode = userPreference ?? 'light'
+  const nextMode = mode === 'light' ? 'dark' : 'light'
+  const modeLabel = {
+    light: 'Light ',
+    dark: 'Dark ',
+  }
 
-	return (
-		<fetcher.Form method="POST" {...form.props}>
-			<input type="hidden" name="theme" value={nextMode} />
-			<div className="flex gap-2 text-white">
-				<button
-					name="intent"
-					value="update-theme"
-					type="submit"
-					className="flex h-8 w-8 cursor-pointer items-center justify-center"
-				>
-					{modeLabel[mode]}
-				</button>
-			</div>
-			<ErrorList errors={form.errors} id={form.errorId} />
-		</fetcher.Form>
-	)
+  return (
+    <fetcher.Form method="POST" {...form.props}>
+      <input type="hidden" name="theme" value={nextMode} />
+      <div className="flex gap-2 text-white">
+        <button
+          name="intent"
+          value="update-theme"
+          type="submit"
+          className="flex h-8 w-8 cursor-pointer items-center justify-center"
+        >
+          {modeLabel[mode]}
+        </button>
+      </div>
+      <ErrorList errors={form.errors} id={form.errorId} />
+    </fetcher.Form>
+  )
 }
 
 function ShowToast({ toast }: { toast: unknown }) {
-	const { id, type, title, description } = toast as {
-		id: string
-		type: 'success' | 'message'
-		title: string
-		description: string
-	}
-	useEffect(() => {
-		setTimeout(() => {
-			showToast[type](title, { id, description })
-		}, 0)
-	}, [description, id, title, type])
-	return null
+  const { id, type, title, description } = toast as {
+    id: string
+    type: 'success' | 'message'
+    title: string
+    description: string
+  }
+  useEffect(() => {
+    setTimeout(() => {
+      showToast[type](title, { id, description })
+    }, 0)
+  }, [description, id, title, type])
+  return null
 }
 
 export const meta: MetaFunction = () => {
-	return [
-		{ title: 'Marta Blog' },
-		{
-			name: 'description',
-			description: 'Marta Blog',
-		},
-	]
+  return [
+    { title: 'Marta Blog' },
+    {
+      name: 'description',
+      description: 'Marta Blog',
+    },
+  ]
 }
 
 export function ErrorBoundary() {
-	return (
-		<Document>
-			<GeneralErrorBoundary />
-		</Document>
-	)
+  return (
+    <Document>
+      <GeneralErrorBoundary />
+    </Document>
+  )
 }
