c0197c755c068cc166d8a082b3993aa3ae532fcb
marta00084 <martvonk00084@gmail.com>
2024-01-29 19:25:29 +0100

    .

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,23 +1,14 @@
-/**
- * By default, Remix will handle generating the HTTP Response for you.
- * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
- * For more information, see https://remix.run/file-conventions/entry.server
- */
-
-import type { AppLoadContext, EntryContext } from "@remix-run/cloudflare";
-import { RemixServer } from "@remix-run/react";
-import { isbot } from "isbot";
-import { renderToReadableStream } from "react-dom/server";
+import type { AppLoadContext, EntryContext } from '@remix-run/cloudflare'
+import { RemixServer } from '@remix-run/react'
+import * as isbotModule from 'isbot'
+import { renderToReadableStream } from 'react-dom/server'
 
 export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext,
-  // This is ignored so we can keep it in the template for visibility.  Feel
-  // free to delete this parameter in your app if you're not using it!
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  loadContext: AppLoadContext
+  loadContext: AppLoadContext,
 ) {
   const body = await renderToReadableStream(
     <RemixServer context={remixContext} url={request.url} />,
@@ -25,19 +16,40 @@ export default async function handleRequest(
       signal: request.signal,
       onError(error: unknown) {
         // Log streaming rendering errors from inside the shell
-        console.error(error);
-        responseStatusCode = 500;
+        console.error(error)
+        responseStatusCode = 500
       },
-    }
-  );
+    },
+  )
 
-  if (isbot(request.headers.get("user-agent") || "")) {
-    await body.allReady;
+  if (isBotRequest(request.headers.get('user-agent'))) {
+    await body.allReady
   }
 
-  responseHeaders.set("Content-Type", "text/html");
+  responseHeaders.set('Content-Type', 'text/html')
   return new Response(body, {
     headers: responseHeaders,
     status: responseStatusCode,
-  });
+  })
+}
+
+// We have some Remix apps in the wild already running with isbot@3 so we need
+// to maintain backwards compatibility even though we want new apps to use
+// isbot@4.  That way, we can ship this as a minor Semver update to @remix-run/dev.
+function isBotRequest(userAgent: string | null) {
+  if (!userAgent) {
+    return false
+  }
+
+  // isbot >= 3.8.0, >4
+  if ('isbot' in isbotModule && typeof isbotModule.isbot === 'function') {
+    return isbotModule.isbot(userAgent)
+  }
+
+  // isbot < 3.8.0
+  if ('default' in isbotModule && typeof isbotModule.default === 'function') {
+    return isbotModule.default(userAgent)
+  }
+
+  return false
 }
